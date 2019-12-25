c92f53efff98b7d63c800d6cafce033937e5b4d9
Louis <louisgh.cn@gmail.com>
2019-12-25 09:45:42 +0800

    [update] rename project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('ng-monacolanguage app is running!');
+    expect(page.getTitleText()).toEqual('angular-monaco-languageclient app is running!');
   });
 
   afterEach(async () => {
